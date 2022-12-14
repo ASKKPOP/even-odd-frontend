5ca83c1ecc58d9ecdf26dc293ef27a82f0f894b1
DucLV <cntt0401.luuvietduc@gmail.com>
2022-12-14 14:17:44 +0700

    last

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const BASE_API_URL = 'http://dev-admin.63-ok.com/api'
+const BASE_API_URL = 'http://api.gamepradise.net/api'
 const BASIC_TOKEN = 'c2ltcGxlX3Rva2Vu'
 const ODD_RESULT = 1
 const EVEN_RESULT = 2
@@ -8,6 +8,7 @@ let bettingTime = null;
 let startBetTime = null;
 let isShowTimer = false
 let timmer = null
+let roomId = 1
 
 const CARD_TYPE = [
     '/images/statistic/clover.png',
@@ -28,17 +29,27 @@ const BACKGROUND_RESULT = [
 const HIDDEN_GBG = '/images/statistic/hidden-bg.png'
 const SHOW_GBG = '/images/statistic/show-bg.png'
 
+const gamePlayEle = document.querySelector('.game-play')
+const gameIfr = document.querySelector('#ifr-game')
+
 function start(){
+    roomId = getvalue(window.location.href, 'room_id')
+
+    console.log({roomId})
+    const gameUrl = 'http://cocos.gamepradise.net?room_id=' + roomId
+    const gameIframe = document.getElementById('ifr-game')
+    gameIframe.setAttribute('src', gameUrl)
     onGetInfo()
-    // setInterval(update, 100)
+    setInterval(update, 100)
 }
 
 function update(){
+    gamePlayEle.click()
+    gameIfr.click()
     if (isShowTimer) {
         const now = Date.now()
         if (now < endBetTime) {
             let remainingTime = Math.round((endBetTime - now) / 1000)
-            console.log(remainingTime)
         } else {
             // PopupController.inst.showNotice('Stop betting time')
             isShowTimer = false
@@ -60,10 +71,9 @@ const onGetInfo = async() => {
 }
 
 const onGetFirstCard = () => {
-  const roomId = getvalue(window.location.search, "room_id");
   console.log({ roomId });
   return new Promise(async (resolve, reject) => {
-    fetch(BASE_API_URL + "/round-info/1", {
+    fetch(BASE_API_URL + "/round-info/" + roomId, {
       method: "GET", // or 'PUT'
       headers: {
         "Content-Type": "application/json",
@@ -75,7 +85,7 @@ const onGetFirstCard = () => {
     })
       .then((response) => response.json())
       .then((response) => {
-        console.log("response:", response);
+        // console.log("response:", response);
         if (response.status !== 200) {
           reject(new Error("Have error when trying to get data!"));
         }
@@ -126,7 +136,8 @@ const getvalue = (str, name) => {
 
 function updateSummnery(data) {
     updateResult(data.history)
-   
+    updateStatistic(data.statistic)
+    updateChinaBoard(data.chinaboard)
 }
 
 function updateResult(data) {
@@ -162,7 +173,101 @@ function updateResult(data) {
     bodyTable.innerHTML = bodyStr
 }
 
+function updateStatistic(data) {
+    const statisticMap = [
+        {title: '10 게임', key: 'last_10_rounds'},
+        {title: '20 게임', key: 'last_20_rounds'},
+        {title: '30 게임', key: 'last_30_rounds'},
+        {title: '100 게임', key: 'last_100_rounds'},
+        {title: '1 DAY', key: 'last_1_days'},
+        {title: '2 DAYS', key: 'last_2_days'}
+    ]
 
+    const bodyStr = statisticMap.map(item => {
+        return `<tr>
+            <td>${item.title}</td>
+            <td>
+                <div class="tatistic-bg dot blue-dot"><span class="">${data[item.key]['odd']}</span></div>
+            </td>
+            <td>
+                <div class="tatistic-bg dot red-dot"><span class="">${data[item.key]['even']}</span></div>
+            </td>            
+        </tr>`
+    }).join('');
+
+    const bodyTable = document.querySelector('#tbl-statistic tbody')
+    bodyTable.innerHTML = bodyStr
+
+}
+
+
+
+function updateChinaBoard(data) {
+    console.log('width', window.innerWidth)
+	console.log({data})
+    let i = 0
+    let contentStr = ''
+    if(window.innerWidth <= 526){
+        if(data.length > 28){
+            const removeCount = data.length - 28;
+            data.splice(0, removeCount)
+            console.log(data)
+        }
+    }
+    while (i < data.length) {
+        const columnData = data[i]
+        // console.log('column,',i,columnData)
+        if (columnData.length < 1) return
+		let colStr = ''
+		const colTitle = columnData[0] == EVEN_RESULT ? '짝' : '홀'
+		const colClass = columnData[0] == EVEN_RESULT ? 'red' : 'blue'
+        for (let j = 0; j < 6; j++) {
+			let itemClass = ''
+			if(columnData[j] == ODD_RESULT){
+				itemClass = 'odd'
+			}
+			if(columnData[j] == EVEN_RESULT){
+				itemClass = 'even'
+			} 
+			colStr += `<div class="china-item ${itemClass}"><span class="china-dot"></span></div>`
+        }
+		contentStr += `<div class="china-column"><div class="col-title ${colClass}">${colTitle}</div>${colStr}</div>`
+        i++
+    }
+
+	const chinaContent = document.querySelector('.game-chinaboard .content')
+	chinaContent.innerHTML = contentStr
+
+    // if (data.length < 1) return
+    // boardView.removeAllChildren()
+    // if(data[0][0] == EVEN_RESULT){
+    //     data.splice(0, 1) //neu cot dau la chan thi bat dau tu cot thu 2, do anh bat dau tu le
+    // }
+    // let i = 0
+    // while (i < data.length) {
+    //     const columnData = data[i]
+    //     console.log('column,',i,columnData)
+    //     if (columnData.length < 1) return
+    //     const columnNode = cc.instantiate(boardColumnPrefab)
+    //     for (let j = 0; j < columnData.length; j++) {
+    //         if ([ODD_RESULT, EVEN_RESULT].includes(columnData[j])) {
+    //             let itemNode = null
+    //             if(columnData[j] == ODD_RESULT){
+    //                 itemNode = cc.instantiate(boardOddItemPrefab)
+    //             }
+    //             if(columnData[j] == EVEN_RESULT){
+    //                 itemNode = cc.instantiate(boardEvenItemPrefab)
+    //             } 
+    //             itemNode.parent = columnNode
+    //             itemNode.y = 87.253 - 35.61 * j
+    //             itemNode.x = -0.093
+    //         }
+    //     }
+    //     columnNode.parent = boardView
+    //     columnNode.x = 35.4 * i - 654
+    //     i++
+    // }
+}
 
 function getCardSuit(suit) {
     if (suit == "spade") {
